531a20fb6b9c7e1cddad2ac707ebb474bf40e283
dielisdelen <dielisdelen@gmail.com>
2026-06-08 13:17:47 +0200

    E17: first result, save to file

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from pypdf import PdfReader
 reader = PdfReader("test.pdf")
 text = reader.pages[0].extract_text()
 text = text.replace("Jansen", "[REDACTED]")
+with open("output.txt", "w") as f:
+    f.write(text)
 print(text)
