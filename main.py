5b1f2376679a517c72e4614fb103b67a8ff2bf39
dielisdelen <dielisdelen@gmail.com>
2026-06-08 15:40:22 +0200

    E23: detect license plates, handle multiple formats

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ text = text.replace("Jansen", "[REDACTED]")
 def redact_nine_digit_numbers(text):
     return re.sub(r"\b\d{9}\b", "[REDACTED]", text)
 
+def redact_license_plates(text):
+    pattern = r"\b(?:[A-Z]{2}-\d{2}-\d{2}|\d{2}-\d{2}-[A-Z]{2}|\d{2}-[A-Z]{2}-\d{2}|[A-Z]{2}-\d{2}-[A-Z]{2}|[A-Z]{2}-[A-Z]{2}-\d{2}|\d{2}-[A-Z]{2}-[A-Z]{2}|\d{2}-[A-Z]{3}-\d|\d-[A-Z]{3}-\d{2}|[A-Z]{2}-\d{3}-[A-Z]|[A-Z]-\d{3}-[A-Z]{2})\b"
+    return re.sub(pattern, "[REDACTED]", text)
+
 text = redact_nine_digit_numbers(text)
+text = redact_license_plates(text)
 with open("output.txt", "w") as f:
     f.write(text)
 print(text)
